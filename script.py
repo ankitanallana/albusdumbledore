41330d3dcd495aebd8fd5f5597d8ef2453670b17
Ankita Nallana <ankita.nallana@gmail.com>
2019-10-24 13:52:24 -0700

    Add coding challenge

---
+++ script.py
@@ -0,0 +1,10 @@
+# Write a program to print the words of a string backwards
+# For eg: "it's a beautiful day" should be "day beautiful a it's"
+
+def reverseTheString(str):
+    # Write your program here
+    # Return the string
+
+
+s = "the quick brown fox jumps over the lazy dog"
+print(reverseTheString(s))
